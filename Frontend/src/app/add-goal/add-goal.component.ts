5a499e906c4e89e0ee764c9d67cf6632af424a00
Andy Zou <azou@xpanxion.com>
2022-09-29 14:34:34 -0400

    Balance and Monthly Payment mentods

--- Frontend/src/app/add-goal/add-goal.component.ts
+++ Frontend/src/app/add-goal/add-goal.component.ts
@@ -30,7 +30,19 @@ export class AddGoalComponent implements OnInit {
   }
   
   onSubmit(){
+    this.calculateBalance();
+    this.calculateMonthlyPayment();
+    console.log(this.goal.userId+"TEST");
     console.log(this.goal);
     this.saveGoal();
+
   }
+  calculateBalance(){
+    this.goal.balance = this.goal.cost - this.goal.downPayment;
+  }
+  calculateMonthlyPayment(){
+    this.goal.userId = this.goal.cost * ((this.goal.interest*Math.pow((1+this.goal.interest),this.goal.months))/(Math.pow((1+this.goal.interest),this.goal.months) - 1))
+  }
+
+
 }
