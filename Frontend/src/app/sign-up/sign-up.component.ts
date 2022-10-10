9fce14c7e8a4f6194d50601edd58ae21ac170fd9
nomadic8280 <99271852+nomadic8280@users.noreply.github.com>
2022-10-10 09:38:14 -0700

    Added user registration

--- Frontend/src/app/sign-up/sign-up.component.ts
+++ Frontend/src/app/sign-up/sign-up.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../user';
+import { UserService } from '../user.service';
+
 
 @Component({
-  selector: 'app-sign-up',
+  selector: 'app-add-user',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+ 
 
-  constructor() { }
+
+  user: User = new User();
+
+  constructor(private userService: UserService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  saveUser(){
+    this.userService.createUser(this.user).subscribe( data =>{
+      this.router.navigate(['/goals']);
+    },
+    
+    error => console.log(error));
+  }
+
+  
+  onSubmit(){
+    this.saveUser();
+
+  }
 }
